2857af58542173ae10d373b082038021b7575f83
Ross Reicks <rreicks@tractorzoom.com>
2021-12-22 11:00:29 -0600

    feat: add react native paper

--- App.tsx
+++ App.tsx
@@ -1,13 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { BottomNavigation, Button, DefaultTheme, Text, Provider as PaperProvider, Appbar } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1f6ef3',
+    accent: '#0e1c36',
+  },
+};
+
+const MusicRoute = () => (
+  <View style={styles.container}>
+    <Text>Search</Text>
+    <StatusBar style="auto" />
+  </View>
+)
+
+const AlbumsRoute = () => (
+  <View style={styles.container}>
+    <Text>Recents</Text>
+    <StatusBar style="auto" />
+  </View>
+)
+
+const RecentsRoute = () => (
+  <View style={styles.container}>
+    <Text>Albumbs</Text>
+    <StatusBar style="auto" />
+  </View>
+)
 
 export default function App() {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'music', title: 'Music', icon: 'camera' },
+    { key: 'albums', title: 'Albums', icon: 'album' },
+    { key: 'recents', title: 'Recents', icon: 'history' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    music: MusicRoute,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+  });
+
+  const _goBack = () => console.log('Went back');
+
+  const _handleSearch = () => console.log('Searching');
+
+  const _handleMore = () => console.log('Shown more');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+          <Appbar.Header>
+      <Appbar.BackAction onPress={_goBack} />
+      <Appbar.Content title="Title" subtitle="Subtitle" />
+      <Appbar.Action icon="magnify" onPress={_handleSearch} />
+      <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+    </Appbar.Header>
+      <BottomNavigation
+          navigationState={{ index, routes }}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
+    </PaperProvider>
   );
 }
 
